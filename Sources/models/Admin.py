f334092f5a8fb5031b3bb3600ff7bbefe8161d85
pablo <ribcukpavlo15@gmail.com>
2025-05-16 12:14:44 +0300

    Creating object classes in models folders.

--- Sources/models/Admin.py
+++ Sources/models/Admin.py
@@ -4,4 +4,35 @@ from .Project import Project
 
 
 class Admin(Human):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.__action_history = []
+        self.__projects = []
+        self.notes = None
+
+    @property
+    def action_history(self):
+        return self.__action_history
+
+    @action_history.setter
+    def action_history(self, action: object):
+        if not isinstance(action, Action):
+            raise TypeError("Action must be an instance")
+        self.__action_history.append(action)
+
+    @property
+    def projects(self):
+        return self.__projects
+
+    @projects.setter
+    def projects(self, project: object):
+        if not isinstance(project, Project):
+            raise TypeError("Project must be an instance")
+        self.__projects.append(project)
+
+    def remove_project(self, project_id: str):
+        for project in self.__projects:
+            if project.project_id() == project_id:
+                self.__projects.remove(project)
+                return True
+        raise IndexError("Project not found")
--- Sources/models/Employee.py
+++ Sources/models/Employee.py
@@ -2,4 +2,12 @@ from .Human import *
 
 
 class Employee(Human):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.position = None
+        self.rate = 0
+
+    def show_info(self):
+        base_info = super().show_info()
+        return base_info + (f"\nPosition: {self.position if self.position else "not mentioned"};\n"
+                            f"Rate: {self.rate};\n")
